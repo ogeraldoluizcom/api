bfcd86376f32f75bb8095dfc82ace002b8b20479
Geraldo Luiz <geraldobl58@gmail.com>
2025-06-02 17:05:01 -0300

    first commit

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/health (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/health')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toEqual({
+          status: 'Server is running',
+          timestamp: expect.any(String), // Verifica se o timestamp é uma string
+        });
+      });
+  });
+});
