bd9f31c4728286b117ae69e917e59020a614289a
Geraldo Luiz <geraldobl58@gmail.com>
2025-06-04 17:35:15 -0300

    feat: update AppModule to include CasesModule and CasesController; add unit tests for CasesController and CasesService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { HealthModule } from './health/health.module';
+import { CasesController } from './cases/cases.controller';
+import { CasesModule } from './cases/cases.module';
 
 @Module({
-  imports: [HealthModule],
-  controllers: [],
+  imports: [ConfigModule.forRoot(), HealthModule, CasesModule],
+  controllers: [CasesController],
+  providers: [],
 })
 export class AppModule {}
