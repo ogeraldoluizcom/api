3ecba42034043f1a83390488dabeff80447225d5
Geraldo Luiz <geraldobl58@gmail.com>
2025-06-04 14:27:23 -0300

    refactor: remove unused logger and throttler configurations, and clean up app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,9 @@
-import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { LoggerModule } from 'nestjs-pino';
 
-import { CustomThrottlerGuard } from './guards/custom-throttler/custom-throttler.guard';
 import { HealthModule } from './health/health.module';
 
-import { loggerConfig } from './utils/logger-config';
-import { throttlersConfig } from './utils/throttlers-config';
-
 @Module({
-  imports: [
-    LoggerModule.forRoot(loggerConfig),
-    ThrottlerModule.forRoot(throttlersConfig),
-    HealthModule,
-  ],
+  imports: [HealthModule],
   controllers: [],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: CustomThrottlerGuard,
-    },
-  ],
 })
 export class AppModule {}
-
-// Resultado:
-// Se você configurar ttl: 60 e limit: 10, o cliente
-// poderá fazer até 10 requisições por minuto (60 segundos).
-// Após atingir o limite, ele precisará esperar até o final do
-// período de 60 segundos para que o contador seja resetado.
--- src/utils/throttlers-config.ts
+++
@@ -1,8 +0,0 @@
-export const throttlersConfig = {
-  throttlers: [
-    {
-      ttl: 120000, // Tempo de vida em milissegundos (2 minutos)
-      limit: 10, // Máximo de 10 requisições por IP
-    },
-  ],
-};
