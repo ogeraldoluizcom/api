3e9498375ff4bee474124223f0352b5bd3755e11
Geraldo Luiz <geraldobl58@gmail.com>
2025-06-06 11:02:41 -0300

    feat(cases): enhance CreateCaseDto and CaseEntity with cover, gallery, and techs fields; improve error handling in CasesRepository

--- src/cases/repositories/cases.repository.ts
+++ src/cases/repositories/cases.repository.ts
@@ -1,73 +1,134 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
-import { PrismaService } from '../../prisma/prisma.service';
 import { CreateCaseDto } from '../dto/create-case.dto';
 import { UpdateCaseDto } from '../dto/update-case.dto';
+
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class CasesRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(createCaseDto: CreateCaseDto) {
-    const createCase = await this.prisma.case.create({
-      data: {
-        ...createCaseDto,
-      },
-    });
+  async create(
+    createCaseDto: CreateCaseDto & {
+      cover: string;
+      gallery: string[];
+    },
+  ) {
+    try {
+      const createCase = await this.prisma.case.create({
+        data: {
+          ...createCaseDto,
+          techs:
+            typeof createCaseDto.techs === 'string'
+              ? (createCaseDto.techs as string).split(',').map((t) => t.trim())
+              : createCaseDto.techs,
+        },
+      });
 
-    if (!createCase) {
-      throw new Error('Case creation failed');
+      return {
+        statusCode: HttpStatus.CREATED,
+        message: 'Case created successfully.',
+        data: createCase,
+      };
+    } catch {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Failed to create case.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-
-    return createCase;
   }
 
   async findAll() {
-    const cases = await this.prisma.case.findMany();
+    try {
+      const cases = await this.prisma.case.findMany();
 
-    if (!cases) {
-      throw new Error('No cases found');
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Cases retrieved successfully.',
+        data: cases,
+      };
+    } catch {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Failed to retrieve cases.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-
-    return cases;
   }
 
   async findOne(id: string) {
-    const caseData = await this.prisma.case.findUnique({
-      where: { id },
-    });
-
-    if (!caseData) {
-      throw new Error(`Case with ID ${id} not found`);
+    try {
+      const caseData = await this.prisma.case.findUnique({
+        where: { id },
+      });
+      return {
+        statusCode: HttpStatus.OK,
+        message: `Case with ID ${id} found successfully.`,
+        data: caseData,
+      };
+    } catch {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.NOT_FOUND,
+          message: `Case with ID ${id} not found.`,
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
-
-    return caseData;
   }
 
   async update(id: string, updateCaseDto: UpdateCaseDto) {
-    const updatedCase = await this.prisma.case.update({
-      where: { id },
-      data: {
-        ...updateCaseDto,
-      },
-    });
-
-    if (!updatedCase) {
-      throw new Error(`Case with ID ${id} not found or update failed`);
+    try {
+      const updatedCase = await this.prisma.case.update({
+        where: { id },
+        data: {
+          ...updateCaseDto,
+          techs:
+            typeof updateCaseDto.techs === 'string'
+              ? (updateCaseDto.techs as string).split(',').map((t) => t.trim())
+              : updateCaseDto.techs,
+        },
+      });
+      return {
+        statusCode: HttpStatus.OK,
+        message: `Case with ID ${id} updated successfully.`,
+        data: updatedCase,
+      };
+    } catch {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.NOT_FOUND,
+          message: `Case with ID ${id} not found or updated failed.`,
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
-
-    return updatedCase;
   }
 
   async remove(id: string) {
-    const deletedCase = await this.prisma.case.delete({
-      where: { id },
-    });
+    try {
+      await this.prisma.case.delete({
+        where: { id },
+      });
 
-    if (!deletedCase) {
-      throw new Error(`Case with ID ${id} not found or deletion failed`);
+      return {
+        statusCode: HttpStatus.NO_CONTENT,
+        message: `Case with ID ${id} deleted successfully.`,
+      };
+    } catch {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.NOT_FOUND,
+          message: `Case with ID ${id} not found or deletion failed.`,
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
-
-    return deletedCase;
   }
 }
