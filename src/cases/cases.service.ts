cb3a2fa4d8020b79f88343fb4fc369ba3651ad1b
Geraldo Luiz <geraldobl58@gmail.com>
2025-06-08 13:11:14 -0300

    refactor(cases): simplify constructor and improve DTO handling in create and update methods

--- src/cases/cases.service.ts
+++ src/cases/cases.service.ts
@@ -3,17 +3,17 @@ import { Injectable } from '@nestjs/common';
 import { CreateCaseDto } from './dto/create-case.dto';
 import { UpdateCaseDto } from './dto/update-case.dto';
 import { CasesRepository } from './repositories/cases.repository';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class CasesService {
-  constructor(
-    private readonly repository: CasesRepository,
-    private readonly prisma: PrismaService,
-  ) {}
+  constructor(private readonly repository: CasesRepository) {}
 
   async create(createCaseDto: CreateCaseDto) {
-    const { cover = '', gallery = [], ...rest } = createCaseDto as any;
+    const {
+      cover = '',
+      gallery = [],
+      ...rest
+    } = createCaseDto as CreateCaseDto;
     return this.repository.create({
       ...rest,
       cover,
@@ -30,7 +30,11 @@ export class CasesService {
   }
 
   async update(id: string, updateCaseDto: UpdateCaseDto) {
-    const { cover = '', gallery = [], ...rest } = updateCaseDto as any;
+    const {
+      cover = '',
+      gallery = [],
+      ...rest
+    } = updateCaseDto as CreateCaseDto;
 
     return this.repository.update(id, {
       ...rest,
